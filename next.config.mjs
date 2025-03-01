af30f2e1121ce46e3c45215a164e63f64206f64f
Hamza Burhan <123304211+hamza-burhan@users.noreply.github.com>
2025-03-01 14:12:14 +0500

    image optimize issue

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   output: 'export', // Add this line for static export
+  unoptimized: true, // Disable image optimization
 };
 
 export default nextConfig;
